1f79c75b9bea106c91cc42f06948dddc8a1fa99f
kopE <158080477+edokope4@users.noreply.github.com>
2026-05-07 22:36:44 -0400

    Add simple traffic light program in semaforo-simple.ino

---
+++ examples/semaforo-simple.ino
@@ -0,0 +1,57 @@
+/**
+* --- Semárofo simple ---
+*/
+
+/*
+* Definición de constantes para todo el programa
+* tiempos de duración encendido del Rojo para detenerse
+* tiempos de duración encendido del Amarillo para precaución y cautela
+* tiempos de duración encendido del Verde para continuar
+*/
+const int tiempoRojo     = 5000;
+const int tiempoAmarillo = 1000;
+const int tiempoVerde    = 7000;
+
+// verde
+const int LED_VERDE    = 2;
+// amarillo
+const int LED_AMARILLO = 3;
+// rojo
+const int LED_ROJO     = 4;
+
+// Configuramos los leds
+void setup() {
+  pinMode(LED_VERDE, OUTPUT);
+  // amarillo
+  pinMode(LED_AMARILLO, OUTPUT);
+  // rojo
+  pinMode(LED_ROJO, OUTPUT);
+}
+
+// vamos y vamos y vamos y vamos.....
+void loop() {
+  turnoVerde();
+  turnoAmarillo();
+  turnoRojo();  
+}
+
+// Encender y apagar el verde
+void turnoVerde() {
+  digitalWrite(LED_VERDE, HIGH);
+  delay(tiempoVerde);
+  digitalWrite(LED_VERDE, LOW);
+}
+
+// Encender y apagar el amarillo
+void turnoAmarillo() {
+  digitalWrite(LED_AMARILLO, HIGH);
+  delay(tiempoAmarillo);
+  digitalWrite(LED_AMARILLO, LOW);
+}
+
+// Encender y apagar el rojo
+void turnoRojo() {
+  digitalWrite(LED_ROJO, HIGH);
+  delay(tiempoRojo);
+  digitalWrite(LED_ROJO, LOW);
+}
